718877574221a43f214e8167bb11dc7e234652a5
78Istvan <istvan78juhasz@gmail.com>
2021-07-10 09:17:28 +0000

    first js code

---
+++ assets/js/folder.js
@@ -0,0 +1,16 @@
+var d = new Date();
+theDay = d.getDay();
+switch(theDay){
+    case 5:
+        ducument.write('<br>Finally Friday</br>');
+        break;
+    case 6:
+        document.write('<br>Super Saturday</br>');
+        break;
+    case 0:
+        document.write('<br>Sleepy Sunday</br>');
+        break;
+    default:
+        document.write('<br>Can this week over</br>');
+
+}
