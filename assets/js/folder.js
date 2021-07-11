72875d681bc7eefb25885f0c115d66060aa313f6
78Istvan <istvan78juhasz@gmail.com>
2021-07-11 11:27:09 +0000

    alert buttons with javacsript

--- assets/js/folder.js
+++ assets/js/folder.js
@@ -11,10 +11,38 @@ switch(theDay){
         document.write('<br>Sleepy Sunday</br>');
         break;
     default:
-        document.write('<br>Can this week over</br>');
+        document.write('<br><h1>Can this week over</h1></br>');
 
 }
-
+//alert pop up window message
 function show_alert() {
     alert('Hello! I am an alert box!')
 }
+
+//confirm message pop up window
+
+function show_confirm() {
+    var r = confirm('Press a button!');
+    if (r == true) 
+    {
+    alert('You pressed Ok!');
+    }
+     else 
+     {
+    alert('You pressed cancel!');
+    }
+}
+
+//prompt messages pop up window
+function show_prompt() {
+    var name = prompt('Please enter your name','');
+    if (name != null && name != '') {
+        document.write('Hello '+ name + '! How are you today');
+    }
+}
+
+//breaking text line in the pop up windows
+function breakText() {
+    alert("I will berak this text here"+'\n'+"because I want to do this here!")
+}
+
